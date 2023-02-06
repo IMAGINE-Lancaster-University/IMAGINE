d3e86ff3b1f631f23eb987b6702fcf0bc066eeb8
Isaac Wetton <isaac@wetton.net>
2023-02-06 15:28:24 +0000

    Completed script
    For each data point, the script checks whether the position is within the specified range and if so outputs that data point to the output.txt file.

--- Data Python Scripts/JSOpositiondata/JSOpositiondata.py
+++ Data Python Scripts/JSOpositiondata/JSOpositiondata.py
@@ -11,7 +11,7 @@ import os
 # Galileo position x, y, z (December 2000 Ganymede Flyby)
 galPos = (-15.2, 0.57, 0.84)
 # Allowed ranges
-allowedErr = (0.2, 0.05, 0.05)
+allowedErr = (2, 0.4, 0.4)
 
 # Define input and output files
 inputFile = open("input.txt", "r")
@@ -22,8 +22,16 @@ outputFile = open("output.txt", "x")
 # Read input data into a list
 lsInput = inputFile.readlines()
 
-for i in range(1, 207359):
+for i in range(1, 207357):
     dataPoint = lsInput[i].split("     ")
+    if galPos[0] - allowedErr[0] < float(dataPoint[1]) < galPos[0] + allowedErr[0] \
+            and galPos[1] - allowedErr[1] < float(dataPoint[2]) < galPos[1] + allowedErr[1] \
+            and galPos[2] - allowedErr[2] < float(dataPoint[3]) < galPos[2] + allowedErr[2]:
+        output = ""
+        for x in dataPoint:
+            output += " " + x
+        outputFile.write(output)
 
+# Close input and output files
 inputFile.close()
 outputFile.close()

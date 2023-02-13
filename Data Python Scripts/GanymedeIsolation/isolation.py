592e2b830a475e1dbcae9dddbdbf35c400c27ffc
Isaac Wetton <isaac@wetton.net>
2023-02-13 11:49:38 +0000

    Added isolation.py with initial script comments

---
+++ Data Python Scripts/GanymedeIsolation/isolation.py
@@ -0,0 +1,9 @@
+# Author: Isaac Wetton
+# Contact: isaac@wetton.net
+
+# Given ASCII .txt input for December and May, the script will:
+# Reformat the time so that it is seconds since the beginning of the timeseries.
+# Average the magnetic field for timeseries point for each month.
+# Calculate the average deviation between the two plots (excluding Ganymede encounter).
+# Adjust the May plot using these average values so that it is the same magnitude as the December plot.
+# Subtract the May data points from the December data points.

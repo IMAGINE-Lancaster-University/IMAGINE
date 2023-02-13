a622cb41d9ce88e01b35830436d7e1eaa1a9920f
Isaac Wetton <isaac@wetton.net>
2023-02-13 16:24:53 +0000

    Create Table object for each dataset and calculate average magnetic field for each recorded time point

--- Data Python Scripts/GanymedeIsolation/main.py
+++ Data Python Scripts/GanymedeIsolation/main.py
@@ -4,4 +4,14 @@
 # Imports
 from isolation import *
 
+# Create data tables for May 2000 and December 2000 data from text files
+dec = Table(file="dec.txt")
+may = Table(file="may.txt")
 
+# Format the time to seconds since beginning of timeseries
+dec.timeformat()
+may.timeformat()
+
+# Average magnetic field strength for each time point
+mayAv = may.collectav_bmag()
+decAv = dec.collectav_bmag()

b4c3d69b0e79968bc3adf40ef6f634c1d280268e
Isaac Wetton <isaac@wetton.net>
2023-02-13 11:50:09 +0000

    Update JSOpositiondata.py
    Added author and contact details

--- Data Python Scripts/JSOpositiondata/JSOpositiondata.py
+++ Data Python Scripts/JSOpositiondata/JSOpositiondata.py
@@ -1,3 +1,6 @@
+# Author: Isaac Wetton
+# Contact: isaac@wetton.net
+
 # This script will analyse a dataset with format:
 # DATA_COLUMNS : SECS FROM 1970-01-01, gll_xyz_jso[0], gll_xyz_jso[1], gll_xyz_jso[2],
 # gll_mag_msreal_Mag_7523700765402304842, gll_r_jup_7523700765402304842, gll_orb_ga_RANGE_SPRH_7523700765402304842

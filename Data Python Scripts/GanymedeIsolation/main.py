91a5da78bfb86f4ab58f0230ca0efeaac3746a38
Isaac Wetton <isaac@wetton.net>
2023-02-20 15:12:05 +0000

    Adjusted ignored time range in average calculation

--- Data Python Scripts/GanymedeIsolation/main.py
+++ Data Python Scripts/GanymedeIsolation/main.py
@@ -27,7 +27,7 @@ for i in mayAv:
 # Average the differences in diffAv
 totalDiffs = 0
 for diff in diffAv:
-    if float(diff[0]) < 6800 or float(diff[0]) > 7600:
+    if float(diff[0]) < 7050 or float(diff[0]) > 7890:
         totalDiffs += float(diff[1])
 adjustment = totalDiffs / len(diffAv)
 
